a64d629990393ea37fa34e7ce2391a0b81e9ea03
Jeroen van Paridon <jerpar@mpi.nl>
2018-05-11 16:41:33 +0200

    Random cleanup

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
 # -*- coding: utf-8 -*-
 # jvparidon@gmail.com
-import strip_xml
-import join_txts
-import count_words
+import sub2vec
--- count_words.py
+++ count_words.py
@@ -1,19 +1,39 @@
 # -*- coding: utf-8 -*-
 # jvparidon@gmail.com
 import os
+import argparse
 
 
 def count_words(filename):
     with open(filename, 'r') as infile:
         wordcount = 0
         for line in infile:
-            wordcount += len(line.split())
+            wordcount += len(line.split(' '))
         return wordcount
+
+def print_count(wordcount, filename):
+    if wordcount > 10000000000:
+        print('{: >6}B words in file {}'.format(int(wordcount / 1000000000), filename))
+    elif wordcount > 10000000:
+        print('{: >6}M words in file {}'.format(int(wordcount / 1000000), filename))
+    elif wordcount > 10000:
+        print('{: >6}K words in file {}'.format(int(wordcount / 1000), filename))
+    else:
+        print('{: >6}  words in file {}'.format(wordcount, filename))
 
 
 if __name__ == '__main__':
-    folder = 'OpenSubtitles2018/raw'
-    for filename in os.listdir(folder):
-        if filename.endswith('.txt'):
-            wordcount = count_words(os.path.join(folder, filename))
-            print('{}M words in file {}'.format(int(wordcount / 1000000), filename))
+    argparser = argparse.ArgumentParser(description='count words in files in a given directory')
+    argparser.add_argument('--directory', default='../OpenSubtitles2018/raw', help='directory the files are located in')
+    argparser.add_argument('--filename', help='filename (overrides directory argument)')
+    args = argparser.parse_args()
+
+    if args.filename:
+        wordcount = count_words(args.filename)
+        print_count(wordcount, args.filename)
+    else:
+        folder = args.directory
+        for filename in sorted(os.listdir(folder)):
+            if '.' in filename:
+                wordcount = count_words(os.path.join(folder, filename))
+                print_count(wordcount, filename)
