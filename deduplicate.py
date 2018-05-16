9fa21190e3a7fed2c07e742b8bd0921cca70dec9
Jeroen van Paridon <Jeroen.vanParidon@mpi.nl>
2018-05-16 22:26:19 +0200

    added routine for reddit corpus deduplication

--- deduplicate.py
+++ deduplicate.py
@@ -1,31 +1,38 @@
 # -*- coding: utf-8 -*-
 # jvparidon@gmail.com
 import os
+import lzma
 from utensilities import timer
 
 
-def get_lines(fname, verbose=True):
-    with open(fname, 'r') as training_file:
-        lines = training_file.read().split('\n')
-        n_lines = len(lines)
-        lines = set(lines)
-        n_duplicates = n_lines - len(lines)
+def get_lines(fhandle):
+    lines = fhandle.read().split('\n')
+    n_lines = len(lines)
+    lines = set(lines)
+    n_duplicates = n_lines - len(lines)
     return lines, n_lines, n_duplicates
-
-
-def write_dedup(fname, lines):
-    with open(fname, 'w') as dedup_file:
-        dedup_file.write('\n'.join(lines))
 
 
 @timer
 def dedup_file(in_fname, out_fname):
-    lines, n_lines, n_duplicates = get_lines(in_fname)
-    write_dedup(out_fname, lines)
+    with open(in_fname, 'r') as in_file, open(out_fname, 'w') as out_file:
+        lines, n_lines, n_duplicates = get_lines(in_file)
+        out_file.write('\n'.join(lines))
     return n_lines, n_duplicates
+
+
+def dedup_reddit(folder, verbose=False):
+    fnames = [os.path.join(folder, fname) for fname in sorted(list(os.listdir(folder))) if fname.endswith('_stripped.xz')]
+    for fname in fnames:
+        with lzma.open(fname, 'rt') as in_file, open('{}.dedup.txt'.format(fname[:-3]), 'w') as out_file:
+            lines, n_lines, n_duplicates = get_lines(in_file)
+            out_file.write('\n'.join(lines))
+        if verbose:
+            print('removed {} duplicates from {} lines in file {}'.format(n_duplicates, n_lines, fname))
 
 
 if __name__ == '__main__':
     # for deduplicating reddit corpus without memory overruns, deduplicate by month first, then by year, then overall?
-    n_lines, n_duplicates, t = dedup_file('../training_data/opensubtitles/raw/en.txt', '../dedup.en.txt')
-    print('read {} lines and removed {} duplicates in {} seconds'.format(n_lines, n_duplicates, int(t['duration'])))
+    #n_lines, n_duplicates, t = dedup_file('../training_data/opensubtitles/raw/en.txt', '../dedup.en.txt')
+    #print('read {} lines and removed {} duplicates in {} seconds'.format(n_lines, n_duplicates, int(t['duration'])))
+    dedup_reddit('../reddit', verbose=True)
