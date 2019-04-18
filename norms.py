c86f33876aaf4f111292f3fb0b3430df18f248ab
JvParidon <jvparidon@gmail.com>
2019-04-19 00:18:52 +0200

    Added evaluate_vecs method to norms.py

--- norms.py
+++ norms.py
@@ -6,6 +6,7 @@ import sklearn.linear_model
 import sklearn.model_selection
 import sklearn.preprocessing
 import argparse
+import os
 import vecs
 from utensilities import log_timer
 import logging
@@ -13,28 +14,52 @@ logging.basicConfig(format='[{levelname}] {message}', style='{', level=logging.I
 
 
 @log_timer
-def predict_norms(vectors, norms):
+def evaluate_vecs(lang, vecs_fname):
+    vectors = vecs.Vectors(vecs_fname, normalize=True, n=1e6).as_df()
+    results = []
+    for norms_fname in os.listdir('evaluation/norms'):
+        if norms_fname.startswith(lang):
+            results.append(predict_norms(vectors, norms_fname))
+    results_fname = os.path.split(vecs_fname)[1].replace('.vec', '.tsv')
+    pd.concat(results).to_csv(os.path.join('results/norms', results_fname), sep='\t')
 
+
+@log_timer
+def predict_norms(vectors, norms_fname):
+    norms = pd.read_csv(norms_fname, sep='\t')
     norms = norms.set_index('words')
     df = norms.join(vectors, how='left').dropna()
 
-    scaler = sklearn.preprocessing.StandardScaler()
-    model = sklearn.linear_model.Ridge()
-    X = scaler.fit_transform(df[vectors.columns.values])
+    scaler = sklearn.preprocessing.StandardScaler()  # standardize predictors
+    model = sklearn.linear_model.Ridge()  # use ridge regression models
+    X = scaler.fit_transform(df[vectors.columns.values])  # word vectors are the predictors
 
+    results = []
     for col in norms.columns.values:
+        # set dependent variable and calculate 10-fold mean fit/predict scores
         y = df['ratings']
-        scores = sklearn.model_selection.cross_val_score(model, X, y, cv=10)
-        print(col)
-        print(np.mean(scores))
+        score = np.mean(sklearn.model_selection.cross_val_score(model, X, y, cv=10))
+        results.append({
+            'source': norms_fname.rstrip('.tsv'),
+            'norm': col,
+            'r': np.sqrt(score),  # take square root of explained variance to get Pearson r
+            'r-squared': score
+        })
+
+    return pd.DataFrame(results)
+
+
+
+@log_timer
+def extend_norms(vecs_fname, norms_fname):
+    # implement extension of lexical norms using pandas and ridge regression fit/predict
+    pass
 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description='perform crossvalidated penalized regression of lexical norms using word vectors as predictors')
+    argparser.add_argument('lang')
     argparser.add_argument('vecs_fname')
-    argparser.add_argument('norms_fname')
     args = argparser.parse_args()
 
-    norms = pd.read_csv('aoa-en-kuperman-2012.csv')
-    vectors = vecs.Vectors(args.vecs_fname, normalize=True, n=2e5).as_df()
-    predict_norms(vectors, norms)
+    evaluate_vecs(args.lang, args.vecs_fname)
