adfac4d75deb4a993c2771231aa539f39732818b
JvParidon <jvparidon@gmail.com>
2019-09-06 16:23:46 +0200

    Reconfigured readthedocs conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,14 +19,20 @@ sys.path.insert(0, os.path.abspath('../'))
 project = 'subs2vec'
 copyright = '2019, Jeroen van Paridon & Bill Thompson'
 author = 'Jeroen van Paridon & Bill Thompson'
-master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode'
+]
+
+source_suffix = '.rst'
+master_doc = 'index'
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -42,7 +48,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
+
+html_sidebars = {
+   '**': ['globaltoc.html', 'sourcelink.html',
+          'searchbox.html', 'relations.html']
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
