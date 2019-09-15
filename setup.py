e3298ef14cd3c758ec7ee9a6e706ef90cf378f40
JvParidon <jvparidon@gmail.com>
2019-09-15 22:02:56 +0200

    Up version number to 0.9.1

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from distutils.core import setup
+import distutils.core
 
-setup(name='subs2vec',
-      version='0.9.0',
-      description='subs2vec module',
-      author='Jeroen van Paridon & Bill Thompson',
-      author_email='jvparidon@gmail.com',
-      url='https://github.com/jvparidon/subs2vec/',
-      packages=['subs2vec'],
-      package_data={'subs2vec': ['*/*.tsv']},
+distutils.core.setup(
+    name='subs2vec',    
+    version='0.9.1',
+    description='subs2vec module',
+    author='Jeroen van Paridon & Bill Thompson',
+    author_email='jvparidon@gmail.com',
+    url='https://github.com/jvparidon/subs2vec/',
+    packages=['subs2vec'],
+    package_data={'subs2vec': ['*/*.tsv']},
 )
