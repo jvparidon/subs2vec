15614f526b58c02bcbebe58564b40b6ec6050d57
JvParidon <jvparidon@gmail.com>
2019-10-15 10:52:12 +0200

    switched install to setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,39 @@
-import distutils.core
+import setuptools
+import os
 
-distutils.core.setup(
-    name='subs2vec',    
-    version='0.9.1',
-    description='subs2vec module',
-    author='Jeroen van Paridon & Bill Thompson',
-    author_email='jvparidon@gmail.com',
-    url='https://github.com/jvparidon/subs2vec/',
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as readme:
+    long_description = readme.read()
+
+
+setuptools.setup(
+    name='subs2vec',
+    version='0.9.2',
     packages=['subs2vec'],
-    package_data={'subs2vec': ['datasets/*/*.tsv', 'paper_results/*/*.tsv']},
+    package_data={
+        'subs2vec': ['datasets/*/*.tsv', 'paper_results/*/*.tsv'],
+    },
+    author='Jeroen van Paridon & Bill Thompson',
+    author_email='j.v.paridon@gmail.com',
+    url='https://github.com/jvparidon/subs2vec/',
+    description='subs2vec module',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas>=0.25',
+        'lxml',
+        'joblib',
+        'matplotlib',
+        'seaborn>=0.9',
+        'scikit-learn',
+        'psutil',
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
 )
--- subs2vec/__init__.py
+++ subs2vec/__init__.py
@@ -3,8 +3,8 @@ Provides:
   - Routines for interacting with, evaluating, and training word embeddings as used in Van Paridon & Thompson (2019).
   - Routines for extending lexical norms to unnormed words.
   - Routines for retrieving word, bigram, and trigram frequencies.
-  - Command line tools to conveniently do any of the above, in 50 languages.
+  - Command line tools to conveniently do any of the above, in 55 languages.
 """
 
 __author__ = 'Jeroen van Paridon & Bill Thompson'
-__version__ = '0.9.1'
+__version__ = '0.9.2'
